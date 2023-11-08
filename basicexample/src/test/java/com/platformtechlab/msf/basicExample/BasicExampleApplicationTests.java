d8dc906549bbf25060632c80b4e4a0a4287e4909
319 <319@DESKTOP-B919VUN>
2023-11-08 15:49:35 +0900

    프로젝트

---
+++ basicexample/src/test/java/com/platformtechlab/msf/basicExample/BasicExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.platformtechlab.msf.basicExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ clientexample/src/main/java/com/platformtechlab/msf/clientExample/ClientExampleApplication.java
@@ -0,0 +1,13 @@
+package com.platformtechlab.msf.clientExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientExampleApplication.class, args);
+	}
+
+}
